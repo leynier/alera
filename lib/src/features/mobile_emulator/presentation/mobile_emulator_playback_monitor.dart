51147ab3dd5d28bffa6d987d189734754c495ce1
Leynier Gutiérrez González <leynier41@gmail.com>
2026-07-28 22:43:48 -0600

    fix: stabilize mobile emulator playback

--- lib/src/features/mobile_emulator/presentation/mobile_emulator_playback_monitor.dart
+++ lib/src/features/mobile_emulator/presentation/mobile_emulator_playback_monitor.dart
@@ -1,13 +1,49 @@
 import 'dart:async';
 
+enum MobileEmulatorPlaybackRecoveryAction { retry, fail }
+
+class MobileEmulatorPlaybackRecoveryPolicy {
+  MobileEmulatorPlaybackRecoveryPolicy({
+    this.stabilityWindow = const Duration(seconds: 30),
+  });
+
+  final Duration stabilityWindow;
+  Timer? _stabilityTimer;
+  bool _automaticRetryUsed = false;
+
+  void playbackStarted() {
+    _stabilityTimer?.cancel();
+    _stabilityTimer = Timer(stabilityWindow, reset);
+  }
+
+  MobileEmulatorPlaybackRecoveryAction recordFailure() {
+    _stabilityTimer?.cancel();
+    _stabilityTimer = null;
+    if (_automaticRetryUsed) {
+      return MobileEmulatorPlaybackRecoveryAction.fail;
+    }
+    _automaticRetryUsed = true;
+    return MobileEmulatorPlaybackRecoveryAction.retry;
+  }
+
+  void reset() {
+    _stabilityTimer?.cancel();
+    _stabilityTimer = null;
+    _automaticRetryUsed = false;
+  }
+
+  void dispose() => reset();
+}
+
 class MobileEmulatorPlaybackMonitor {
   MobileEmulatorPlaybackMonitor({
     required Stream<String> errors,
     required Stream<bool> completions,
+    required this.onWarning,
     required this.onFailure,
     this.retryDelay = const Duration(milliseconds: 500),
   }) {
-    _errorSubscription = errors.listen((_) => _scheduleFailure());
+    _errorSubscription = errors.listen(_reportWarning);
     _completionSubscription = completions.listen((completed) {
       if (completed) {
         _scheduleFailure();
@@ -15,12 +51,22 @@ class MobileEmulatorPlaybackMonitor {
     });
   }
 
+  final void Function(String warning) onWarning;
   final void Function() onFailure;
   final Duration retryDelay;
   late final StreamSubscription<String> _errorSubscription;
   late final StreamSubscription<bool> _completionSubscription;
   Timer? _failureTimer;
+  bool _warningReported = false;
   bool _disposed = false;
+
+  void _reportWarning(String warning) {
+    if (_disposed || _warningReported) {
+      return;
+    }
+    _warningReported = true;
+    onWarning(warning);
+  }
 
   void _scheduleFailure() {
     if (_disposed || _failureTimer != null) {
