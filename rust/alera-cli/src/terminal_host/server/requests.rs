f58a74c451f7f94f581fcca1a7691db57f1c6006
Leynier Gutiérrez González <leynier41@gmail.com>
2026-08-22 17:51:52 -0600

    fix: remove stale relay import

--- rust/alera-cli/src/terminal_host/server/requests.rs
+++ rust/alera-cli/src/terminal_host/server/requests.rs
@@ -22,7 +22,6 @@ use crate::terminal_host::protocol::{
 };
 use crate::terminal_host::session::SessionDriver;
 
-use super::mobile_gateway_surface::mobile_request_allowed;
 pub(super) use super::request_payloads::{json_result, parse_payload};
 use super::runtime_mutation_barrier::conflicts_with_runtime_mutation;
 use super::{ClientKind, ServerActor, ServerCommand};
