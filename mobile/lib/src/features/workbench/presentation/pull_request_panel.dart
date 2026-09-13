b2cb236c9cc3b12cc0e2bcb20b8ca9c91dba1184
Leynier Gutiérrez González <leynier41@gmail.com>
2026-09-13 01:45:07 -0600

    fix: import AleraRefreshProgress on the pull request panel

--- mobile/lib/src/features/workbench/presentation/pull_request_panel.dart
+++ mobile/lib/src/features/workbench/presentation/pull_request_panel.dart
@@ -5,6 +5,7 @@ import 'package:alera_mobile/src/design_system/badges/alera_badge.dart';
 import 'package:alera_mobile/src/design_system/buttons/alera_icon_button.dart';
 import 'package:alera_mobile/src/design_system/feedback/alera_empty_state.dart';
 import 'package:alera_mobile/src/design_system/feedback/alera_notice.dart';
+import 'package:alera_mobile/src/design_system/feedback/alera_refresh_progress.dart';
 import 'package:alera_mobile/src/design_system/icons/alera_icons.dart';
 import 'package:alera_mobile/src/design_system/layout/alera_section_header.dart';
 import 'package:alera_mobile/src/features/runtime/domain/mobile_pull_request_actions.dart';
